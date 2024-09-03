9dd775947af089867e384b63a874210f4305f1bc
unknown <alhaqyrobbi@gmail.com>
2024-09-03 21:27:56 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+const texts = ["Photographer", "UI/UX Desaigner", "Designer"]; // Daftar kata
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+(function type() {
+    if (count === texts.length) {
+        count = 0; 
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.sec-text').textContent = letter;
+
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+        setTimeout(type, 2000);
+    } else {
+        setTimeout(type, 150); 
+    }
+})();
+
+document.getElementById('menu-icon').addEventListener('click', function() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+});
+
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (scrollTop > lastScrollTop) {
+        header.style.top = '-100px';
+    } else {
+        header.style.top = '0';
+    }
+    
+    lastScrollTop = scrollTop;
+});
+
+const video = document.querySelector('.background-video');
+video.addEventListener('ended', function() {
+    video.currentTime = 0;
+    video.play();
+});
+
+
+// salin clipboard dari contact
+// telpon
+document.getElementById('click-phone').addEventListener('click', function(event) {
+    const phoneNumber = document.getElementById('phone-no').innerText;
+    navigator.clipboard.writeText(phoneNumber).then(() => {
+        alert('Nomor telepon berhasil disalin!');
+    }).catch(err => {
+        console.error('Gagal menyalin teks: ', err);
+    });
+});
+
+// email
+document.getElementById('click-gmail').addEventListener('click', function(event) {
+    const phoneNumber = document.getElementById('gmail').innerText;
+    navigator.clipboard.writeText(phoneNumber).then(() => {
+        alert('Email berhasil disalin!');
+    }).catch(err => {
+        console.error('Gagal menyalin teks: ', err);
+    });
+});
